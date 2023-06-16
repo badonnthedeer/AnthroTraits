0ea8a2f1188a107fc0bf5f86f360971a8037e97e
Badonn <badonnthedeer@protonmail.com>
2023-06-16 18:45:40 -0400

    Alphabetize trait declarations

--- media/lua/shared/NPCs/AnthroTraitsMainCreationMethods.lua
+++ media/lua/shared/NPCs/AnthroTraitsMainCreationMethods.lua
@@ -97,48 +97,83 @@ local function initAnthroTraits()
     --TTF.Add("Mechanics2", "Human,Anthro");
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+    TraitFactory.addTrait("AT_BeastOfBurden", getText("UI_trait_AT_BeastOfBurden"), 1, getText("UI_trait_AT_BeastOfBurden_desc"), false);
+    TTF.Add("AT_NaturalTumbler", "Anthro,Strong");
+
+    TraitFactory.addTrait("AT_Bug-o-ssieur", getText("UI_trait_AT_Bug-o-ssieur"), 1, getText("UI_trait_AT_Bug-o-ssieur_desc"), false);
+    --TTF.Add("AT_Bug-o-ssieur", "Human,Anthro");
+
+    TraitFactory.addTrait("AT_BullRush", getText("UI_trait_AT_BullRush"), 6, getText("UI_trait_AT_BullRush_desc"), false);
+    TTF.Add("AT_BullRush", "Anthro,Horns");
+
+    TraitFactory.addTrait("AT_Carnivore", getText("UI_trait_AT_Carnivore"), 2, getText("UI_trait_AT_Carnivore_desc"), false);
+    TTF.Add("AT_Carnivore", "Anthro,Carnivore");
+
+    TraitFactory.addTrait("AT_CarrionEater", getText("UI_trait_AT_CarrionEater"), 3, getText("UI_trait_AT_CarrionEater_desc"), false);
+    TTF.Add("AT_CarrionEater", "Anthro,Carnivore,Scavenger");
+
+    TraitFactory.addTrait("AT_Digitigrade", getText("UI_trait_AT_Digitigrade"), 3, getText("UI_trait_AT_Digitigrade_desc"), false);
+    TTF.Add("AT_Digitigrade", "Anthro");
+
+    TraitFactory.addTrait("AT_Exclaimer", getText("UI_trait_AT_Exclaimer"), -6, getText("UI_trait_AT_Exclaimer_desc"), false);
+    --TTF.Add("AT_Exclaimer", "Human,Anthro");
+
+    local AT_FeralBody = TraitFactory.addTrait("AT_FeralBody", getText("UI_trait_AT_FeralBody"), 1, getText("UI_trait_AT_FeralBody_desc"), false);
+    AT_FeralBody:addXPBoost(Perks.Strength, 1);
+    AT_FeralBody:addXPBoost(Perks.Fitness, -1);
+    TTF.Add("AT_FeralBody", "Anthro");
+
+    TraitFactory.addTrait("AT_FeralDigestion", getText("UI_trait_AT_FeralDigestion"), -4, getText("UI_trait_AT_FeralDigestion_desc"), false);
+    TTF.Add("AT_FeralDigestion", "Anthro");
+
+    TraitFactory.addTrait("AT_FoodMotivated", getText("UI_trait_AT_FoodMotivated"), 1, getText("UI_trait_AT_FoodMotivated_desc"), false);
+    TTF.Add("AT_FoodMotivated", "Anthro");
+
+    TraitFactory.addTrait("AT_Herbivore", getText("UI_trait_AT_Herbivore"), 2, getText("UI_trait_AT_Herbivore_desc"), false);
+    TTF.Add("AT_Herbivore", "Anthro,Herbivore");
+
     local AT_Hooves = TraitFactory.addTrait("AT_Hooves", getText("UI_trait_AT_Hooves"), 4, getText("UI_trait_AT_Hooves_desc"), false);
     AT_Hooves:addXPBoost(Perks.Sprinting, 1);
     AT_Hooves:addXPBoost(Perks.Nimble, 1);
     TTF.Add("AT_Hooves", "Anthro,Hooves");
+
+    TraitFactory.addTrait("AT_Immunity", getText("UI_trait_AT_Immunity"), 4, getText("UI_trait_AT_Immunity_desc"), false);
+    TTF.Add("AT_Immunity", "Anthro");
+    --add ability to ignore this trait if infected by a former (furry) player?
+
+    TraitFactory.addTrait("AT_Lonely", getText("UI_trait_AT_Lonely"), -4, getText("UI_trait_AT_Lonely_desc"), false);
+    TTF.Add("AT_Lonely", "Anthro,Social"); --(Xochi suggestion)
+
+    TraitFactory.addTrait("AT_NaturalTumbler", getText("UI_trait_AT_NaturalTumbler"), 3, getText("UI_trait_AT_NaturalTumbler_desc"), false);
+    TTF.Add("AT_NaturalTumbler", "Anthro,Agile");
 
     local AT_Paws = TraitFactory.addTrait("AT_Paws", getText("UI_trait_AT_Paws"), 2, getText("UI_trait_AT_Paws_desc"), false);
     AT_Paws:addXPBoost(Perks.Lightfoot, 1);
     AT_Paws:addXPBoost(Perks.Sneak, 1);
     TTF.Add("AT_Paws", "Anthro,Paws");
 
-    local AT_Body = TraitFactory.addTrait("AT_FeralBody", getText("UI_trait_AT_FeralBody"), 1, getText("UI_trait_AT_FeralBody_desc"), false);
-    AT_Body:addXPBoost(Perks.Strength, 1);
-    AT_Body:addXPBoost(Perks.Fitness, -1);
-    TTF.Add("AT_FeralBody", "Anthro");
-
+    TraitFactory.addTrait("AT_Stinky", getText("UI_trait_AT_Stinky"), -4, getText("UI_trait_AT_Stinky_desc"), false);
+    --TTF.Add("AT_Stinky", "Human,Anthro");
 
     TraitFactory.addTrait("AT_Tail", getText("UI_trait_AT_Tail"), 2, getText("UI_trait_AT_Tail_desc"), false);
     TTF.Add("AT_Tail", "Anthro,Tail");
 
-    TraitFactory.addTrait("AT_Immunity", getText("UI_trait_AT_Immunity"), 4, getText("UI_trait_AT_Immunity_desc"), false);
-    TTF.Add("AT_Immunity", "Anthro");
-    --add ability to ignore this trait if infected by a former (furry) player?
-
-    TraitFactory.addTrait("AT_Herbivore", getText("UI_trait_AT_Herbivore"), 2, getText("UI_trait_AT_Herbivore_desc"), false);
-    TTF.Add("AT_Herbivore", "Anthro,Herbivore");
-    TraitFactory.addTrait("AT_Carnivore", getText("UI_trait_AT_Carnivore"), 2, getText("UI_trait_AT_Carnivore_desc"), false);
-    TTF.Add("AT_Carnivore", "Anthro,Carnivore");
-    TraitFactory.addTrait("AT_CarrionEater", getText("UI_trait_AT_CarrionEater"), 3, getText("UI_trait_AT_CarrionEater_desc"), false);
-    TTF.Add("AT_CarrionEater", "Anthro,Carnivore,Scavenger");
-    TraitFactory.addTrait("AT_FoodMotivated", getText("UI_trait_AT_FoodMotivated"), 1, getText("UI_trait_AT_FoodMotivated_desc"), false);
-    TTF.Add("AT_FoodMotivated", "Anthro");
-    TraitFactory.addTrait("AT_FeralDigestion", getText("UI_trait_AT_FeralDigestion"), -4, getText("UI_trait_AT_FeralDigestion_desc"), false);
-    TTF.Add("AT_FeralDigestion", "Anthro");
-    TraitFactory.addTrait("AT_Bug-o-ssieur", getText("UI_trait_AT_Bug-o-ssieur"), 1, getText("UI_trait_AT_Bug-o-ssieur_desc"), false);
-    --TTF.Add("AT_Bug-o-ssieur", "Human,Anthro");
-    -- (see ?lark?)TraitFactory.addTrait("AT_Diurnal", getText("UI_trait_AT_Diurnal"), 1, getText("UI_trait_AT_Diurnal_desc"), false);
-    -- (see night owl)TraitFactory.addTrait("AT_Nocturnal", getText("UI_trait_AT_Nocturnal"), 1, getText("UI_trait_AT_Nocturnal_desc"), false);
     TraitFactory.addTrait("AT_Torpor", getText("UI_trait_AT_Torpor"), -4, getText("UI_trait_AT_Torpor_desc"), false);
     TTF.Add("AT_Torpor", "Anthro,Hibernator");
 
-    TraitFactory.addTrait("AT_Digitigrade", getText("UI_trait_AT_Digitigrade"), 3, getText("UI_trait_AT_Digitigrade_desc"), false);
-    TTF.Add("AT_Digitigrade", "Anthro");
+    TraitFactory.addTrait("AT_UnwieldyHands", getText("UI_trait_AT_UnwieldyHands"), -4, getText("UI_trait_AT_UnwieldyHands_desc"), false);
+    TTF.Add("AT_UnwieldyHands", "Anthro,ThreeFingers");
+
+    TraitFactory.addTrait("AT_VestigialWings", getText("UI_trait_AT_VestigialWings"), 5, getText("UI_trait_AT_VestigialWings_desc"), false);
+    TTF.Add("AT_VestigialWings", "Anthro,Winged");
+
+
+
+
+    -- (see ?lark?)TraitFactory.addTrait("AT_Diurnal", getText("UI_trait_AT_Diurnal"), 1, getText("UI_trait_AT_Diurnal_desc"), false);
+
+    -- (see night owl)TraitFactory.addTrait("AT_Nocturnal", getText("UI_trait_AT_Nocturnal"), 1, getText("UI_trait_AT_Nocturnal_desc"), false);
+
     --TraitFactory.addTrait("AT_KeenSmell", getText("UI_trait_AT_KeenSmell"), 4, getText("UI_trait_AT_KeenSmell_desc"), false);
     --TTF.Add("AT_KeenSmell", "Anthro,KeenSmell");
     -- (vision can't be updated via lua)TraitFactory.addTrait("AT_KeenEyes", getText("UI_trait_AT_KeenEyes"), 1, getText("UI_trait_AT_KeenEyes_desc"), false);
@@ -150,28 +185,20 @@ local function initAnthroTraits()
     -- TraitFactory.addTrait("AT_WannaGoCar", getText("UI_trait_AT_WannaGoCar"), 1, getText("UI_trait_AT_WannaGoCar_desc"), false);
 
     --TraitFactory.addTrait("AT_ColdBlooded", getText("UI_trait_AT_ColdBlooded"), -4, getText("UI_trait_AT_ColdBlooded_desc"), false);
-    TraitFactory.addTrait("AT_Stinky", getText("UI_trait_AT_Stinky"), -4, getText("UI_trait_AT_Stinky_desc"), false);
-    --TTF.Add("AT_Stinky", "Human,Anthro");
-    TraitFactory.addTrait("AT_Exclaimer", getText("UI_trait_AT_Exclaimer"), -6, getText("UI_trait_AT_Exclaimer_desc"), false);
-    --TTF.Add("AT_Exclaimer", "Human,Anthro");
+
+
     -- TraitFactory.addTrait("AT_Sly", getText("UI_trait_AT_Sly"), 1, getText("UI_trait_AT_Sly_desc"), false);
 
     --Can't dynamically change animation speed. It's everyone gets a sped up anim or none.
     --TraitFactory.addTrait("AT_Slinky", getText("UI_trait_AT_Slinky"), 2, getText("UI_trait_AT_Slinky_desc"), false);
     --TTF.Add("AT_Slinky", "Anthro,Agile");
 
-    TraitFactory.addTrait("AT_UnwieldyHands", getText("UI_trait_AT_UnwieldyHands"), -4, getText("UI_trait_AT_UnwieldyHands_desc"), false);
-    TTF.Add("AT_UnwieldyHands", "Anthro,ThreeFingers");
-    TraitFactory.addTrait("AT_NaturalTumbler", getText("UI_trait_AT_NaturalTumbler"), 3, getText("UI_trait_AT_NaturalTumbler_desc"), false);
-    TTF.Add("AT_NaturalTumbler", "Anthro,Agile");
-    TraitFactory.addTrait("AT_VestigialWings", getText("UI_trait_AT_VestigialWings"), 5, getText("UI_trait_AT_VestigialWings_desc"), false);
-    TTF.Add("AT_VestigialWings", "Anthro,Winged");
-    TraitFactory.addTrait("AT_BeastOfBurden", getText("UI_trait_AT_BeastOfBurden"), 1, getText("UI_trait_AT_BeastOfBurden_desc"), false);
-    TTF.Add("AT_NaturalTumbler", "Anthro,Strong");
-    TraitFactory.addTrait("AT_BullRush", getText("UI_trait_AT_BullRush"), 6, getText("UI_trait_AT_BullRush_desc"), false);
-    TTF.Add("AT_BullRush", "Anthro,Horns");
-    TraitFactory.addTrait("AT_Lonely", getText("UI_trait_AT_Lonely"), -4, getText("UI_trait_AT_Lonely_desc"), false);
-    TTF.Add("AT_Lonely", "Anthro,Social"); --(Xochi suggestion)
+
+
+
+
+
+
     -- TraitFactory.addTrait("AT_NoThoughts", getText("UI_trait_AT_NoThoughts"), 1, getText("UI_trait_AT_NoThoughts_desc"), false);
     -- TraitFactory.addTrait("AT_NotCute", getText("UI_trait_AT_NotCute"), 1, getText("UI_trait_AT_NotCute_desc"), false);
     -- TraitFactory.addTrait("AT_SelfClean", getText("UI_trait_AT_SelfClean"), 1, getText("UI_trait_AT_SelfClean_desc"), false);
