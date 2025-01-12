d0fa4aba9d0d8175daf540008fa3333108ec32e2
BadonnTheDeer <badonnthedeer@protonmail.com>
2025-01-11 22:19:35 -0500

    Add sandbox descriptions and rename item fields for food traits

--- Contents/mods/AnthroTraits/42/media/lua/client/NPCs/AnthroTraitsMain.lua
+++ Contents/mods/AnthroTraits/42/media/lua/client/NPCs/AnthroTraitsMain.lua
@@ -508,10 +508,10 @@ end
 
 
 AnthroTraitsMain.ATOnInitWorld = function()
-    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_Carnivore_Items, "ATCarnivore");
-    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_Herbivore_Items, "ATHerbivore");
-    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_Bug_o_ssieur_Items, "ATInsect");
-    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_FeralDigestion_Items, "ATFeralPoison");
+    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_CarnivoreItems, "ATCarnivore");
+    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_HerbivoreItems, "ATHerbivore");
+    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_Bug_o_ssieurItems, "ATInsect");
+    ATU.AddItemTagToItemsFromSandbox(SandboxVars.AnthroTraits.AT_FeralDigestionItems, "ATFeralPoison");
 
     Colors["LavenderBlush"] = Color.new(1, 229/255, 229/255, 1);
 
